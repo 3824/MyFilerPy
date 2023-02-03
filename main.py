9831dfad17480e8c681b8739784a8b3679ffb098
miyanishi <miyanishikazunori@gmail.com>
2023-02-03 19:36:52 +0900

    add main

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+class MyFiler(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+
+        self.master.geometry("600x400")
+
+        ## toolbar of drives
+        f_toolbar = tk.Frame(self.master)
+        btn1 = tk.Button(f_toolbar, text="sample1")
+        btn1.pack(side=tk.LEFT)
+
+        f_toolbar.pack(fill = tk.X)
+
+# root = tk.Tk()
+
+# root.title("my filer py")
+
+# tree = ttk.Treeview(root)
+
+# parent = tree.insert("", "end", text="parent")
+
+# child = tree.insert(parent, "end", text="child")
+
+# tree.pack()
+# root.mainloop()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = MyFiler(master = root)
+    app.mainloop()
+    
